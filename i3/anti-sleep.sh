949e3e387432abc24b759445288149970bce174c
Danilo Sanches da Silva <silva.danilosanches@gmail.com>
2026-07-20 21:33:12 -0300

    feat: add new configs

--- i3/anti-sleep.sh
+++ i3/anti-sleep.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-
+#
 # Script para impedir suspensão se houver áudio tocando (Pipewire/PulseAudio)
+
+export DISPLAY=:0
+
 while true; do
-  if pactl list sinks | grep -q "State: RUNNING"; then
+  if pactl list sink-inputs | grep -q "Driver:"; then
     xset s reset
+    xset dpms force on 2>/dev/null
   fi
 
   sleep 60
